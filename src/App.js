e5bc1f782778d37dcd0c91a6c57d7e4a8e799ffa
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-03-22 21:47:34 +0430

    add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1> hello world</h1>
       
     </div>
   );
