93bfdc91a1c0d1f6f56ac5130d6e01e455f4d311
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-03-24 11:11:37 +0430

    Build NavBar component and add to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1> hello world</h1>
+      <NavBar/>
       
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import FoodDish from '../assets/images/foodDish.svg'
+
+function NavBar(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid bg-dark ">
+                <a className="navbar-brand ms-2" href="#">
+                    <img src={FoodDish} alt = 'logoImage'/>
+                </a>
+                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className='d-flex mx-auto'>
+                        <ul className="navbar-nav mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active fw-bold text-white" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active fw-bold text-white mx-4" href="#">Advance Search</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active fw-bold text-white" href="#">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div className="d-flex">
+                        <a href='##'> <button className="btn text-white bg-info px-2 mx-2 rounded-pill" type="submit">Log In</button> </a>
+                        <a href='###'> <button className="btn text-white bg-primary rounded-pill" type="submit">Sign Up</button> </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
