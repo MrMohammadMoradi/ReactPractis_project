f9f3a5848d66a9def299274a5e1803958d3d541a
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-03-27 21:59:46 +0430

    add bootstrap-icon, Build Card component and add to App.js and change something in App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import './App.css';
 import NavBar from './components/NavBar';
 import SearchMenu from './components/SearchMenu';
+import Card from './components/Card';
 
 function App() {
+
   return (
     <div className="App">
       <NavBar/>
       <SearchMenu/>
+      
+      <div className='container bg-white px-0 mb-5'>
+          <h3 className='titleSecion text-white bg-dark py-2 ps-4'>New Recipes</h3>
+          <div className='subContainer d-flex flex-wrap'>
+            <Card/>
+            
+          </div>
+        </div>
+      
       
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+
+
+function Card({image, title, cardtext}) {
+    return(
+        <div className="recipesCard py-3 px-2">
+            <img src= {image} alt="PostImag"/>
+            <div className=" bg-danger p-3">
+                <h3 className="Title fs-4">{title}</h3>
+                <p className="Description text-white-50 pb-4 mb-0">{cardtext}</p>
+            </div>
+            <button className="SeeMore bg-warning py-2"> More... </button>
+
+        </div>
+    )
+}
+
+export default Card;
