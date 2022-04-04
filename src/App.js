9c4ddf01241fe017006f3b806413bdac3cb3ea65
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-04-04 23:02:11 +0430

    Install React-Royter-Dom and set to App.js and add NavBar.js to App

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import './App.css';
 import React from 'react';
-import {useState, useEffect} from 'react';
 import NavBar from './components/NavBar';
-import SearchMenu from './components/SearchMenu';
-import Card from './components/Card';
 
 function App() {
-  
-  let [post, setPost] = useState([])
-  let numberOfRecipes = 9;
-
-  useEffect(() => {
-    fetch(`https://api.spoonacular.com/recipes/random?number=${numberOfRecipes}&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5`)
-    .then((res) => res.json())
-    .then((data) =>{ setPost(data.recipes);
-      console.log(post, data.recipes)})
-    .then(()=> {
-      console.log(post);
-    })
-    .catch((erorr) => console.log(erorr))
-    
-  }, []);
-
-  
-
 
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavBar/>
-      <SearchMenu/>
-      
-      <div className='container bg-white px-0 mb-5'>
-          <h3 className='titleSecion text-white bg-dark py-2 ps-4'>New post</h3>
-          <div className='subContainer d-flex flex-wrap'>
-            
-            {
-              post.map(item =>
-                <Card title={item.title} cardtext={item.instructions} image={item.image}/>
-              ) 
-            }
-            
-          </div>
-        </div>
-      
-      
-    </div>
+      <Routes>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import FoodDish from '../assets/images/foodDish.svg'
+import {Link} from 'react-router-dom'
 
 function NavBar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid bg-dark ">
-                <a className="navbar-brand ms-4" href="#">
+                <Link className="navbar-brand ms-4" to="/home">
                     <img src={FoodDish} alt = 'logoImage'/>
-                </a>
+                </Link>
                 
                 <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -16,20 +17,20 @@ function NavBar(){
                     <div className='d-flex mx-auto'>
                         <ul className="navbar-nav mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active fw-bold text-white" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link active fw-bold text-white" aria-current="page" to="/home">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active fw-bold text-white mx-4" href="#">Advance Search</a>
+                                <Link className="nav-link active fw-bold text-white mx-4" to="/Search/Advance">Advance Search</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active fw-bold text-white" href="#">About</a>
+                                <Link className="nav-link active fw-bold text-white" to="/About">About</Link>
                             </li>
                         </ul>
                     </div>
                     
                     <div className="d-flex">
-                        <a href='##'> <button className="btn text-white bg-info px-4 mx-2 rounded-pill" type="submit">Log In</button> </a>
-                        <a href='###'> <button className="btn text-white bg-primary px-4 rounded-pill" type="submit">Sign Up</button> </a>
+                        <Link to='/User/Login'> <button className="btn text-white bg-info px-4 mx-2 rounded-pill" type="submit">Log In</button> </Link>
+                        <Link to='/User/SingUp'> <button className="btn text-white bg-primary px-4 rounded-pill" type="submit">Sign Up</button> </Link>
                     </div>
                 </div>
             </div>
