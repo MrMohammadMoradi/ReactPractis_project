b49c872f8119f14c30013ea40a86d0cc92ae65cd
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-04-11 19:04:55 +0430

    Build Terms Component

---
+++ src/Pages/TermsAndPrivacy.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function TermsAndPrivacy() {
+    return (
+        <div className='container py-4'>
+            Terms & Privacy
+On this page you will find information about our terms of use, privacy policy, and security features.
+
+Use of Services
+By using spoonacular's website or apps, and any of the content or information contained herein, you must agree to the Terms on this page.
+
+No Warranties, No Liability
+spoonacular sources recipes, products, and other data from across the web. We do our best to find recipes, products, and restaurant dishes suitable for many diets—whether vegetarian, vegan, gluten free, dairy free, etc. — but we CANNOT guarantee that the ingredients in a recipe, product, or restaurant dish are safe for your diet. Always read ingredient lists from the ORIGINAL source of a recipe (follow the link to the site/blog from the recipe page) in case an ingredient has been imperfectly extracted from the original source or has been labeled inaccurately in any way. Moreover, it is important that you always read the labels on every product you buy in the supermarket to see if the product could cause an allergic reaction or if it conflicts with your personal or religious beliefs. If you are still not sure after reading the label, contact the manufacturer. Similarly, contact fast food chains/restaurants directly to confirm any food you find via spoonacular is truly suitable for your diet.
+
+We also attempt to estimate the cost and calculate the nutritional information for the recipes, products, and restaurant dishes found on our site. Again, we cannot and do not guarantee the accuracy of this information. Additionally, our nutrition visualizer that suggests that you limit sodium, sugar, etc., and get enough protein, vitamins, and minerals is NOT intended as medical advice. Similarly, our tips found on recipe pages are based on articles we have read from various sources across the web, and are NOT based on any medical training. The team behind spoonacular does not possess any medical qualifications and the information may be found to be incorrect or out of date based on future research. If you need help planning your diet or determining which foods (and recipes, products, and restaurant dishes) are safe for you, contact a registered dietitian, allergist, or another medical professional.
+
+Use at Your Own Risk
+spoonacular is not responsible for any adverse effects or damages that occur because of your use of the website, mobile apps, or any information they provide (e.g. after cooking/consuming a recipe on spoonacular or on any of the sites we link to, after reading information from articles or shared via social media, etc.) To use spoonacular, you must accept all risk and responsibility for your use of our site, app, and any material they contain and information they provide. If you do not agree, please stop using the site/app and contact us to delete your account.
+
+Third Party Sites
+spoonacular's website and apps contain links to third party websites. spoonacular is not responsible or liable for the availability or accuracy of such third party websites or the information, products, or services they provide. spoonacular provides these links as a service and linking to these sites does not imply any endorsement by spoonacular. You assume all risk and responsibility for accessing any third party websites from links provided on spoonacular.
+
+Content You Upload
+By uploading a recipe to spoonacular.com or our mobile apps, you grant spoonacular a royalty-free, perpetual, irrevocable, fully sub-licensable, non-exclusive, worldwide right to use, reproduce, modify, translate, adapt, publish, and create derivative works from your recipe (in whole or in part) and/or to incorporate your recipe in other works in any form, media, or technology, now known or later developed, including for commercial purposes. Your ingredient list, instructions, and image will be sub-licensed under a Creative Commons Attribution License.
+
+You also warrant that you have the right to upload any content that you submit and that in doing so you do not in breach any third party's rights.
+
+Privacy Policy
+We want to make sure you understand what data we collect and why. We will never sell your personal information to third parties. We collect data from you in a few ways:
+
+
+        <br/>
+        <br/>
+        <Link className='mx-auto py-3 px-5 fs-3' to='/User/SignUp'>
+            Back to SignUp
+        </Link>
+
+        <Link className='ms-auto py-3 px-5 fs-3' to='/'>
+            Back to Home
+        </Link>
+        </div>
+    )
+}
+
+export default TermsAndPrivacy
