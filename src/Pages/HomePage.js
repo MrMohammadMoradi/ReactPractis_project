ed39da7154c39773462c2b5b566e146ba8722c05
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-04-13 14:25:00 +0430

    change SearchMenu, HomePage,App,js and App.css

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,41 +1,43 @@
-import React from 'react';
-import {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+
+import HomeBody from '../components/HomeBody';
 import SearchMenu from '../components/SearchMenu';
-import Card from '../components/Card';
 
 function HomePage() {
+// https://api.spoonacular.com/recipes/random?number=9&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5
+// https://api.spoonacular.com/recipes/complexSearch?query=pizza&number=9&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5
+
+    const [findRecipes, setFindRecipes] = useState(`https://api.spoonacular.com/recipes/complexSearch?query=pizza&number=9&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5`)
 
     let [post, setPost] = useState([])
-    let numberOfRecipes = 9;
+    // const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        fetch(`https://api.spoonacular.com/recipes/random?number=${numberOfRecipes}&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5`)
+        // setLoading(true);
+        fetch(findRecipes)
         .then((res) => res.json())
-        .then((data) =>{ setPost(data.recipes);
-        console.log(post, data.recipes)})
-        .then(()=> {
-        console.log(post);
-        })
+        .then((data) =>{ setPost(data.recipes)})
+        .then(() => console.log(post))
         .catch((erorr) => console.log(erorr))
-    
-    }, []);
+        // .finally(() => {
+        //     setLoading(false);
+        // });
 
+    }, [findRecipes]);
+
+
+
+    const SetFR = (api)=> {
+        setFindRecipes(api);
+        console.log(api);
+    }
+    
     return (
     <div className="App">
-        <SearchMenu/>
-        
-        <div className='container bg-white px-0 mb-5'>
-            <h3 className='titleSecion text-white bg-dark py-2 ps-4'>New post</h3>
-            <div className='subContainer d-flex flex-wrap'>
-            {
-                post.map(item =>
-                    <Card title={item.title} cardtext={item.instructions} image={item.image}/>
-                ) 
-            }
-            </div>
-        </div>
+        <SearchMenu ChangeAPI={SetFR}/>
     
-    
+        <HomeBody  posts={post} />
+
     </div>
     )
 }
--- src/components/SearchMenu.js
+++ src/components/SearchMenu.js
@@ -1,17 +1,50 @@
+import { useState } from 'react';
 import MenuBackground from '../assets/images/pizza.jpg'
 
-function SearchMenu(){
+function SearchMenu({ChangeAPI}){
+
+    const [recipesName, setRecipesName] = useState('')
+    const [numberOfRecipes, setNumberOfRecipes] = useState(9)
+
+    
+    function DataHandel(e) {
+        e.preventDefault();
+        console.warn("data", recipesName, numberOfRecipes)
+    }
+    
+    function GetAPI(){
+        ChangeAPI(`https://api.spoonacular.com/recipes/complexSearch?query=${recipesName}&number=${numberOfRecipes}&apiKey=182b3ff3a93e4586bad62cf03d8e3bc5`);
+    }
+
     return(
         <div className='mb-4'>
             <div className='SearchSecion d-flex'>
                 <div className='SearchBox mx-auto mt-5'>
-                    <h3 className='fs-2 pt-4'>Search Recipes of Food</h3>
-                    <input className='nameinput border-0 bg-white mt-4 py-1 ps-3 fs-4' typeof='text' placeholder='Search Recipes ' required/>
-                    <div className='NumberRecipes d-flex justify-content-center pt-4 '>
-                        <p className='me-3 mt-2'> number of recipes : </p>
-                        <input className='border-0 mb-2 ' typeof='text' required/>
-                    </div>
-                    <button className='mt-4 px-5  py-1 fs-5 border-0'> Search </button>
+                    <h3 className='fs-2 pt-4'>
+                        Search Recipes of Food
+                    </h3>
+                    <form onSubmit={DataHandel}>
+                        <input className='nameinput border-0 bg-white mt-4 py-1 ps-3 fs-4' 
+                            type='text' 
+                            placeholder='Search Recipes '
+                            onChange={(e) => setRecipesName(e.target.value)}
+                            required/>
+                        <label className='mt-3'>
+                            number of recipes : 
+                            <input className='border-0 ms-2 mb-2 py-2 px-3 fs-5 '
+                                type='number'
+                                placeholder='1'
+                                onChange={(e) => setNumberOfRecipes(e.target.value)}
+                                required/>
+                        </label>
+                        
+                        <br/>
+                        <button className='mt-4 px-5  py-1 fs-5 border-0'
+                            type='submit'
+                            onClick={GetAPI}> 
+                            Search
+                            </button>
+                    </form>
                 </div>
 
 
