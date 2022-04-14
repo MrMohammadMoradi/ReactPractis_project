fd70fb337c509ab1ca1a183966015de2a5f33536
Mohammad Moradi <mrmohammad.moradi@yahoo.com>
2022-04-14 09:49:36 +0430

    set remover html tag in card component and change to HomeBody component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,14 @@
 
 function Card({image, title, cardtext}) {
 
-    
+    let ModifiedCardText = cardtext.replace(/(<([^>]+)>)/gi, "").trim();
 
     return(
         <div className="recipesCard py-3 px-2">
             <img className="CardImage w-100" src= {image} alt="PostImag" />
             <div className="CardDescription bg-danger p-3">
                 <h3 className="Title fs-4">{title}</h3>
-                <p className="Description text-white-50 pb-4 mb-0">{cardtext} ... </p>
+                <p className="Description text-white-50 pb-4 mb-0">{ModifiedCardText} ... </p>
             </div>
             <button className="SeeMore bg-warning py-2"> More... </button>
 
--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -7,13 +7,13 @@ function HomeBody({posts}) {
     // cardtext={item.instructions} 
 
     return (
-        <div>
-            <div className='container bg-white px-0 mb-5'>
+        <div className='homBody py-5'>
+            <div className='container bg-white px-0 '>
             <h3 className='titleSecion text-white bg-dark py-2 ps-4'>New post</h3>
             <div className='subContainer d-flex flex-wrap'>
             {
                 (posts !== undefined) ? posts.map (item =>
-                    <Card title={item.title} image={item.image}/>)
+                    <Card title={item.title} image={item.image} cardtext={item.summary}/>)
                     : <WhenError/>
             }
             </div>
